0fb7094591029d0f0dca67678b40703519344ec9
BrandonDoggett <45565156+BrandonDoggett@users.noreply.github.com>
2019-11-25 12:26:41 -0600

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username':'Brandon'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Beautiful day in Portland!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
